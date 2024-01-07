3531570d5b61a5deb566c60ba6dee5882cce72fb
Cristian <creature.secret@gmail.com>
2024-01-07 10:36:39 -0300

    add use of dom, events, localstorage and JSON

--- blackjack.js
+++ blackjack.js
@@ -1,5 +1,152 @@
 "use strict";
 
+// Obtener referencias a elementos del DOM
+const botonJugar = document.getElementById('botonJugar');
+const inputNombre = document.getElementById('inputNombre');
+const resultadoDiv = document.getElementById('resultado');
+const accionInput = document.getElementById('accionInput');
+const botonAccion = document.getElementById('botonAccion');
+const gameInput = document.getElementById('gameInput');
+const botonGame = document.getElementById('botonGame');
+
+
+const obtenerAccionJugador = () => {
+    return new Promise(resolve => {
+        const clickHandler = () => {
+            const accion = accionInput.value.toLowerCase().trim();
+            resolve(accion);
+            botonAccion.removeEventListener('click', clickHandler); // Elimina el listener después de la jugada
+        };
+        botonAccion.addEventListener('click', clickHandler);
+    });
+};
+
+// Función para obtener el juego del jugador
+const obtenerGameJugador = () => {
+    return new Promise(resolve => {
+        const clickHandler = () => {
+            const game = gameInput.value.toLowerCase().trim();
+            resolve(game);
+            botonGame.removeEventListener('click', clickHandler);
+        };
+        botonGame.addEventListener('click', clickHandler);
+    });
+};
+
+// Función para cargar el estado del juego desde localStorage
+const cargarEstadoJuego = () => {
+    const estadoGuardado = localStorage.getItem('estadoJuego');
+    return estadoGuardado ? JSON.parse(estadoGuardado) : null;
+};
+
+// Función para guardar el estado del juego en localStorage
+const guardarEstadoJuego = (estadoJuego) => {
+    localStorage.setItem('estadoJuego', JSON.stringify(estadoJuego));
+};
+// Función para guardar el estado del juego en localStorage
+const reiniciarEstadoJuego = () => {
+    localStorage.removeItem('estadoJuego');
+};
+
+
+// Función para manejar el juego
+const jugarPartida = async () => {
+    let main = 'si';
+
+    const ejecutarTurnoJugador = (mensaje) => {
+        mostrarMensaje(mensaje);
+        return obtenerAccionJugador()
+    };
+
+    const turnoJugador = async () => {
+        const { mazo, manoJugador, manoDealer } = cargarEstadoJuego();
+        let continuarJugando = true;
+        while (calcularPuntos(manoJugador) < 21 && continuarJugando) {
+            const accion = await ejecutarTurnoJugador('¿Quieres pedir otra carta? (si/no)');
+            if (accion === 'si') {
+                const nuevaCarta = repartirCarta(mazo);
+                manoJugador.push(nuevaCarta);
+                guardarEstadoJuego({ mazo, manoJugador, manoDealer });
+                mostrarEstado(manoJugador, manoDealer);
+            } else {
+                console.log("no quiero otra carta")
+                continuarJugando = false;
+            }
+        }
+    };
+    const rondaJugador = async () => {
+        mostrarMensaje('¿Quieres volver a jugar? (si/no)');
+        await obtenerGameJugador();
+    };
+
+    while (main === 'si') {
+        borrarMensaje();
+        // Inicia el juego
+        let estadoJuego = cargarEstadoJuego();
+        let mazo;
+        let manoJugador;
+        let manoDealer;
+        if (!estadoJuego) {
+            mazo = mezclarMazo(inicializarMazo());
+            manoJugador = [repartirCarta(mazo), repartirCarta(mazo)];
+            manoDealer = [repartirCarta(mazo), repartirCarta(mazo)];
+            guardarEstadoJuego({mazo, manoJugador, manoDealer});
+        } else {
+            mazo = estadoJuego.mazo;
+            manoJugador = estadoJuego.manoJugador;
+            manoDealer = estadoJuego.manoDealer;
+            guardarEstadoJuego({mazo, manoJugador, manoDealer});
+        }
+        mostrarEstado(manoJugador, manoDealer);
+
+        // Turno del jugador
+        await turnoJugador();
+
+        const nuevoEstado = cargarEstadoJuego();
+        mazo = nuevoEstado.mazo;
+        manoJugador = nuevoEstado.manoJugador;
+        manoDealer = nuevoEstado.manoDealer;
+        // Turno del dealer
+        while (calcularPuntos(manoDealer) < 17) {
+            console.log("jugando dealer")
+            const nuevaCarta = repartirCarta(mazo);
+            manoDealer.push(nuevaCarta);
+            guardarEstadoJuego({ mazo, manoJugador, manoDealer });
+        }
+        console.log("termina turno dealer")
+        // Muestra las manos finales
+        mostrarMensaje(`Mano del jugador: ${manoJugador.join(', ')}. Total de puntos: ${calcularPuntos(manoJugador)}`);
+        mostrarMensaje(`Mano del dealer: ${manoDealer.join(', ')}. Total de puntos: ${calcularPuntos(manoDealer)}`);
+
+        // Determina el resultado del juego
+        const resultado = determinarResultado(manoJugador, manoDealer);
+        mostrarMensaje(resultado);
+
+        reiniciarEstadoJuego();
+
+        // Solicita volver a jugar
+        await rondaJugador();
+        main = gameInput.value.toLowerCase().trim();
+    }
+    mostrarMensaje('Espero que te haya gustado el juego');
+};
+
+// Asignar evento al botón para iniciar el juego
+botonJugar.addEventListener('click', jugarPartida);
+
+// Función para mostrar mensajes en el DOM
+const mostrarMensaje = (mensaje) => {
+    const mensajeDiv = document.createElement('p');
+    mensajeDiv.textContent = mensaje;
+    resultadoDiv.appendChild(mensajeDiv);
+};
+
+// Función para borrar mensajes en el DOM
+const borrarMensaje = () => {
+    resultadoDiv.textContent = "";
+};
+
+
 // Función para inicializar el mazo de cartas
 const inicializarMazo = () => {
     const mazo = [];
@@ -74,48 +221,5 @@ const determinarResultado = (manoJugador, manoDealer) => {
 
 // Función para mostrar el estado del juego
 const mostrarEstado = (manoJugador, manoDealer) => {
-    alert(`Tus cartas: ${manoJugador.join(', ')}. Total de puntos: ${calcularPuntos(manoJugador)}. Carta visible del dealer: ${manoDealer[0]}`);
+    mostrarMensaje(`Tus cartas: ${manoJugador.join(', ')}. Total de puntos: ${calcularPuntos(manoJugador)}. Carta visible del dealer: ${manoDealer[0]}`);
 };
-
-let main = prompt("Este es un simulador de Black Jack, quieres jugar una partida? (si/no)");
-while (main == "si") {
-    // Inicia el juego
-    let mazo = inicializarMazo();
-    mazo = mezclarMazo(mazo);
-    const manoJugador = [repartirCarta(mazo), repartirCarta(mazo)];
-    const manoDealer = [repartirCarta(mazo), repartirCarta(mazo)];
-
-    mostrarEstado(manoJugador, manoDealer);
-
-    let continuarJugando = true;
-    // Turno del jugador
-    while (calcularPuntos(manoJugador) < 21 && continuarJugando) {
-        const accion = prompt('¿Quieres pedir otra carta? (si/no)');
-        if (accion == 'si') {
-            const nuevaCarta = repartirCarta(mazo);
-            manoJugador.push(nuevaCarta);
-            alert(`Has recibido una ${nuevaCarta}.`);
-            mostrarEstado(manoJugador, manoDealer);
-        } else {
-            continuarJugando = false;
-        }
-    }
-
-    // Turno del dealer
-    while (calcularPuntos(manoDealer) < 17) {
-        const nuevaCarta = repartirCarta(mazo);
-        manoDealer.push(nuevaCarta);
-        alert(`El dealer pide otra carta y ha recibido una ${nuevaCarta}.`);
-    }
-
-    // Muestra las manos finales
-    alert(`\nMano del jugador: ${manoJugador.join(', ')}. Total de puntos: ${calcularPuntos(manoJugador)}`);
-    alert(`Mano del dealer: ${manoDealer.join(', ')}. Total de puntos: ${calcularPuntos(manoDealer)}`);
-
-
-    // Determina el resultado del juego
-    const resultado = determinarResultado(manoJugador, manoDealer);
-    alert(`\n${resultado}`);
-    main = prompt("Quieres volver a jugar? (si/no)");
-}
-alert("Espero que te haya gustado el juego");
